ec752eacb610068514795de673c6fdeba84a0f35
sanyamtendable <sanyam.saini@tendable.com>
2025-07-02 00:52:25 +0530

    Navigation implementation.

--- app/src/main/java/com/sam/random/composable/otheractivity/MainActivity.kt
+++ app/src/main/java/com/sam/random/composable/otheractivity/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -21,7 +25,12 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.sam.random.composable.R
+import com.sam.random.composable.bottomnav.BottomNav
 import com.sam.random.composable.ui.theme.RandomComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +44,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 )
                 {
-                    LearnTextAndModifier()
+                    DisplayNavComponent()
                 }
             }
         }
@@ -58,6 +67,32 @@ class MainActivity : ComponentActivity() {
     @Composable
     @Preview(showBackground = true)
     private fun LearnTextAndModifierPreview() {
-        LearnTextAndModifier()
+//        LearnTextAndModifier()
+    }
+
+    @Composable
+    private fun MainScreen(navController: NavController) {
+        Column(verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Main Screen", fontSize = 30.sp)
+            Button(onClick = {navController.navigate("BottomNavigation")}) {
+                Text(text = "Go To Bottom Navigation")
+            }
+        }
+    }
+
+    @Composable
+    private fun DisplayNavComponent() {
+        val navController = rememberNavController()
+
+        NavHost(navController = navController, startDestination = "MainScreen") {
+            composable("MainScreen") {
+                MainScreen(navController)
+            }
+
+            composable("BottomNavigation") {
+                BottomNav()
+            }
+        }
     }
 }
